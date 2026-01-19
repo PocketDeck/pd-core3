f7eba4dd0c26b53bdcf156917130955afab3e6a3
Kilian Chung <kilian.chung@web.de>
2026-01-19 01:28:49 +0100

    outsource handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,39 +5,16 @@ import (
 	"log"
 	"net/http"
 
-	"golang.org/x/net/websocket"
+	"PocketDeck/pd-core3/internal/server"
 )
-
-func wsHandler(ws *websocket.Conn) {
-	defer ws.Close()
-
-	log.Println("New connection")
-
-	for {
-		var msg string
-		err := websocket.Message.Receive(ws, &msg)
-		if err != nil {
-			log.Println("Error receiving message:", err)
-			break
-		}
-		log.Println("Received message:", msg)
-
-		reply := "Hello " + msg
-		err = websocket.Message.Send(ws, reply)
-		if err != nil {
-			log.Println("Error sending message:", err)
-			break
-		}
-	}
-}
 
 func main() {
 	host := flag.String("host", "localhost", "host")
 	port := flag.String("port", "8080", "port")
 	flag.Parse()
 
-	http.Handle("/ws", websocket.Handler(wsHandler))
+	http.Handle("/ws", server.NewWebSocketHandler())
 
-	log.Println("Starting server on", *host+":"+*port)
-	log.Fatal(http.ListenAndServe(*host+":"+*port, nil))
+	log.Println("Starting server on", *host + ":" + *port)
+	log.Fatal(http.ListenAndServe(*host + ":" + *port, nil))
 }
