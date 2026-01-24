d4b1fc25ec02d708b7de67511a69d707d66e120a
Kilian Chung <kilian.chung@web.de>
2026-01-24 21:12:17 +0100

    game types

--- internal/game/game.go
+++ internal/game/game.go
@@ -4,3 +4,18 @@ type Game interface {
 	HandleAction(userID int, payload []byte)
 	State(userID int) any
 }
+
+type GameType string
+
+const (
+	GameUno GameType = "uno"
+)
+
+func NewGame(gameType GameType) Game {
+	switch gameType {
+	case GameUno:
+		return NewUnoGame()
+	default:
+		return nil
+	}
+}
